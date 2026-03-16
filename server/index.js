1024c709ddf611c632c38f5f0d4f233667480869
Mahek Khan <mahekkhan8338@gmail.com>
2026-03-16 19:29:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.post(
 );
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  {origin:"https://intellinoteclient.onrender.com" },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
