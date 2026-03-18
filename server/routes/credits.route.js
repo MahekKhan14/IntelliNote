831361670f505bcd56cae951e83ed175a8e5aa53
Mahek Khan <mahekkhan8338@gmail.com>
2026-03-18 11:59:38 +0530

    Update credits.route.js

--- server/routes/credits.route.js
+++ server/routes/credits.route.js
@@ -1,12 +1,10 @@
 import express from "express"
 import isAuth from "../middleware/isAuth.js"
-import { createCreditsOrder } from "../controllers/credits.controller.js"
-
+import { createCreditsOrder, stripeWebhook } from "../controllers/credits.controller.js"
 
 const creditRouter = express.Router()
 
 creditRouter.post("/order", isAuth, createCreditsOrder)
-
-
+creditRouter.post("/webhook", express.raw({ type: "application/json" }), stripeWebhook) // ✅ ADD THIS
 
 export default creditRouter;
